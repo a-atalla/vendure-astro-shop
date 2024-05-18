0eb555a85137cd8b7b6b0eecc228ee483bf51431
a.atalla <a.3talla@gmail.com>
2024-05-18 15:14:45 +0300

    populate shopping cart from stimulus controller

--- src/pages/api/cart.ts
+++ src/pages/api/cart.ts
@@ -1,7 +1,10 @@
 import type { APIRoute } from "astro";
+
 import { sdk } from "~/graphql/sdk";
 
 export const GET: APIRoute = async ({ cookies }) => {
-	const cart = await sdk.getActiveOrder(undefined, { astroCookies: cookies });
+	const cart = await sdk.getActiveOrder(undefined, {
+		astroCookies: cookies,
+	});
 	return new Response(JSON.stringify(cart.activeOrder));
 };
--- src/scripts/controllers/cart-controller.ts
+++ src/scripts/controllers/cart-controller.ts
@@ -1,40 +1,100 @@
 import { Controller } from "@hotwired/stimulus";
-import type { Order } from "~/graphql/_generated";
+import type { CurrencyCode, Order, OrderLine } from "~/graphql/_generated";
+import { formatPrice } from "~/utils";
 
 export default class extends Controller {
-	static targets = ["count"];
+	static targets = [
+		"count",
+		"cartContainer",
+		"itemList",
+		"subTotal",
+		"lineItemTemplate",
+	];
 	static values = {
 		cart: Object,
+		isOpen: false,
 	};
 
-	// typescript declarations
-	// targets
-	declare readonly hasCountTarget: boolean;
-	declare readonly countTarget: HTMLElement;
-	declare readonly countTargets: HTMLElement[];
-
-	// values
+	// values declaration
 	declare cartValue: Partial<Order>;
-	declare readonly hasCartValue: boolean;
+	declare isOpenValue: boolean;
+	// targets declaration
+	declare readonly countTarget: HTMLElement;
+	declare readonly cartContainerTarget: HTMLElement;
+	declare readonly itemTarget: HTMLElement;
+	declare readonly itemListTarget: HTMLElement;
+	declare readonly subTotalTarget: HTMLElement;
+	declare readonly lineItemTemplateTarget: HTMLElement;
 
 	connect() {
 		this.getCart();
 	}
 
+	async getCart() {
+		const res = await fetch("/api/cart");
+		this.cartValue = await res.json();
+	}
+
+	openCart() {
+		this.cartContainerTarget.classList.remove("hidden");
+		this.cartContainerTarget.classList.add("flex");
+		document.body.style.overflow = "hidden";
+	}
+
+	closeCart() {
+		this.cartContainerTarget.classList.remove("flex");
+		this.cartContainerTarget.classList.add("hidden");
+		document.body.style.overflow = "auto";
+	}
+
 	cartValueChanged() {
-		console.log(this.cartValue);
 		if (this.cartValue?.lines) {
 			this.countTarget.classList.remove("hidden");
 			this.countTarget.classList.add("flex");
 			this.countTarget.innerHTML = `${this.cartValue.lines.length}`;
+
+			this.subTotalTarget.innerHTML = formatPrice(
+				this.cartValue.subTotalWithTax,
+				this.cartValue.currencyCode as CurrencyCode,
+			);
 		} else {
 			this.countTarget.classList.remove("flex");
 			this.countTarget.classList.add("hidden");
 		}
+
+		const html = this.lineItemTemplateTarget.innerHTML;
+		if (this.cartValue?.lines) {
+			this.itemListTarget.innerHTML = "";
+			for (const line of this.cartValue.lines) {
+				const lineItem = this.renderLineItem(html, line);
+				this.itemListTarget.appendChild(lineItem);
+			}
+		}
 	}
 
-	async getCart() {
-		const res = await fetch("/api/cart");
-		this.cartValue = await res.json();
+	renderLineItem(html: string, line: OrderLine): HTMLElement {
+		const lineItem = new DOMParser().parseFromString(html, "text/html").body
+			.firstChild as HTMLElement;
+
+		lineItem
+			.querySelector("#lineItemImage")
+			?.setAttribute("src", line.featuredAsset?.preview as string);
+
+		const nameElement = lineItem.querySelector("#lineItemName") as HTMLElement;
+		nameElement.textContent = line.productVariant?.name;
+		const priceElement = lineItem.querySelector(
+			"#lineItemPrice",
+		) as HTMLElement;
+		priceElement.textContent = formatPrice(
+			line.linePriceWithTax,
+			this.cartValue.currencyCode as CurrencyCode,
+		);
+
+		const qtySelector = lineItem.querySelector(
+			"#lineItemQuantity",
+		) as HTMLSelectElement;
+		qtySelector.value = line.quantity.toString();
+
+		return lineItem;
 	}
 }
